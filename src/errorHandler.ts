fd6291d2fa2e6fda950a400e8e7a6a6d3f1071e4
notoriousisk <ishkineyev@mail.ru>
2025-05-13 00:16:06 +0300

    feat: error handler

--- src/errorHandler.ts
+++ src/errorHandler.ts
@@ -1,13 +1,17 @@
 import type { AppConfig } from "vue";
 
-export const errorHandler: AppConfig['errorHandler'] = (err) => {
-    const error = err instanceof Error
-        ? err.message
-        : typeof err === 'string'
-            ? err
-            : JSON.stringify(err)
-    const root = document.getElementById('app') ?? document.body
-    root.insertAdjacentHTML('beforeend', `
+export const errorHandler: AppConfig["errorHandler"] = (err) => {
+  console.log("ERROR", err);
+  const error =
+    err instanceof Error
+      ? err.message
+      : typeof err === "string"
+      ? err
+      : JSON.stringify(err);
+  const root = document.getElementById("app") ?? document.body;
+  root.insertAdjacentHTML(
+    "beforeend",
+    `
             <div>
                 <p>An unhandled error occurred:</p>
                 <blockquote>
@@ -16,5 +20,6 @@ export const errorHandler: AppConfig['errorHandler'] = (err) => {
                     </code>
                 </blockquote>
             </div>
-        `)
-}
+        `
+  );
+};
