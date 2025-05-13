934554bd01550ebd035511f52c1f62aa00ce2fe7
notoriousisk <ishkineyev@mail.ru>
2025-05-13 21:22:26 +0300

    feat: usetonwallet rollback

--- src/tonconnect/useTonWallet.ts
+++ src/tonconnect/useTonWallet.ts
@@ -5,15 +5,12 @@ import type {
   WalletInfoWithOpenMethod,
 } from "@tonconnect/ui";
 import { useTonConnectUI } from "./useTonConnectUI";
-import { updateWalletAddress } from "@/firebase/firebaseService";
-import { initData, useSignal } from "@telegram-apps/sdk-vue";
 
 export function useTonWallet() {
   const wallet = shallowRef<
     Wallet | (Wallet & WalletInfoWithOpenMethod) | null
   >(null);
   const { tonConnectUI } = useTonConnectUI();
-  const initDataRef = useSignal(initData.state);
 
   onMounted(() => {
     if (tonConnectUI) {
@@ -21,14 +18,6 @@ export function useTonWallet() {
       const unsubscribe = tonConnectUI.onStatusChange(
         (value: ConnectedWallet | null) => {
           wallet.value = value;
-
-          // Store wallet address in the database when connected
-          if (value && value.account && value.account.address) {
-            const telegramId = initDataRef.value?.user?.id?.toString();
-            if (telegramId) {
-              updateWalletAddress(telegramId, value.account.address);
-            }
-          }
         }
       );
       onUnmounted(() => {
